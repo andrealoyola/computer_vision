6a201a7cb02605dbc7f1cc692a75a366afd8c589
Andrea Loyola <andrea.loyolad@ucuenca.edu.ec>
2024-10-03 16:41:07 -0500

    se actualizó

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ let video;
 let flippedVideo;
 // To store the classification
 let label = "";
+let confianza = 0;
 
 // Load the model first
 function preload() {
@@ -15,13 +16,13 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(320, 260);
+  createCanvas(320, 260); //se puede modificar a windowWidth y windowHeight
   // Create the video
   video = createCapture(VIDEO);
-  video.size(320, 240);
+  video.size(320, 240); // se puede poner mismas dimensiones que el canvas
   video.hide();
 
-  flippedVideo = ml5.flipImage(video);
+  //flippedVideo = ml5.flipImage(video);
   // Start classifying
   classifyVideo();
 }
@@ -29,24 +30,32 @@ function setup() {
 function draw() {
   background(0);
   // Draw the video
-  image(flippedVideo, 0, 0);
+  image(video, 0, 0);
 
   // Draw the label
-  fill(255);
+  fill(255); //relleno texto
   textSize(16);
   textAlign(CENTER);
   text(label, width / 2, height - 4);
+
+  textAlign(LEFT);
+  textSize(8);
+  text(confianza, 10, height - 4);
+
+  if ((label = "crema")) {
+    filter(INVERT);
+  }
 }
 
 // Get a prediction for the current video frame
 function classifyVideo() {
-  flippedVideo = ml5.flipImage(video);
-  classifier.classify(flippedVideo, gotResult);
-  flippedVideo.remove();
+  //flippedVideo = ml5.flipImage(video);
+  classifier.classify(video, gotResult);
+  //flippedVideo.remove();
 }
 
 // When we get a result
-function gotResult(error, results) {
+function gotResult(results, error) {
   // If there is an error
   if (error) {
     console.error(error);
@@ -55,6 +64,7 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
+  confianza = results[0].confidence;
   // Classifiy again!
   classifyVideo();
 }
